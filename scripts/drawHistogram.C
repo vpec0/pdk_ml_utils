d60b90813816db7ee92de50d8b88b0d53f336acd
Viktor Pec <viktor.pec@fzu.cz>
2023-03-31 15:56:04 +0200

    Changes to draw 2d histograms from all 3 planes.

--- scripts/drawHistogram.C
+++ scripts/drawHistogram.C
@@ -7,7 +7,9 @@
 
 static int gEvent = 0;
 TFile* gF = 0;
+TCanvas* gC = 0;
 const char* gLogfname = 0;
+
 
 void next(int step = 1);
 
@@ -17,6 +19,16 @@ void drawHistogram(const char* fname, const char* logfname, int startevent = 0)
     gLogfname = logfname;
     gF = TFile::Open(fname, "read");
 
+    int padh = 9*34;
+    int padw = 16*34;
+    gC = new TCanvas("c", "", padw+2, 3*padh+24);
+    // Set right margin so that colour scale fits in with its title
+    gC->Divide(1,3,1e-4,1e-4);
+    FOR(i, 3) {
+	auto pad = (TPad*)gC->GetPad(i+1);
+	pad->SetRightMargin(1.2*pad->GetRightMargin());
+    }
+
     next(0);
     return;
 }
@@ -24,20 +36,30 @@ void drawHistogram(const char* fname, const char* logfname, int startevent = 0)
 
 void next(int step)
 {
-    const char* histname = "hCollectionRawDigitsVsWireVsTick_";
+    const char* histname = "Event_%d/hRawDigitsVsWireVsTick_plane%d_%d";
 
     gEvent += step;
     if (gEvent < 0) {
 	gEvent = 0;
 	return;
     }
-    auto h = (TH2F*)gF->Get(Form("%s%d", histname, gEvent));
-    if (!h) {
-	return;
-    }
-    My::SetColorScale(h);
 
-    h->Draw("colz");
+
+    auto setScale = new TExec("setScale", "change_current_hist_color_scale()");
+    FOR(iplane, 3) {
+	auto h = (TH2F*)gF->Get(Form(histname, gEvent, iplane, gEvent));
+	if (!h) {
+	    continue;
+	}
+
+	auto pad = gC->cd(iplane+1);
+	pad->Clear();
+	h->Draw("colz");
+	setScale->Draw();
+	h->Draw("colz same");
+	h->GetZaxis()->SetTitle("Raw ADC");
+	h->SetTitle(Form("Plane %d", iplane));
+    }
 
     // print out event info
     gSystem->Exec(Form("scripts/print_evt_info.sh %d %s", gEvent, gLogfname));
@@ -50,4 +72,16 @@ void previous(int step = 1)
 {
     next(-step);
     return;
+}
+
+void change_current_hist_color_scale()
+{
+    if (!gPad) return;
+    TIter next(gPad->GetListOfPrimitives());
+    while (auto obj = next()) {
+	if (obj->InheritsFrom(TH2::Class())) {
+	    My::SetColorScale((TH2*)obj);
+	    return;
+	}
+    }
 }
