f4c4998309d9b25f1a231791e5a18953bc1087da
Viktor Pec <viktor.pec@fzu.cz>
2024-03-20 13:39:04 +0100

    Implemented reading of 3 planes. Commented out torch related stuff. (Don't want to install anything.)

--- scripts/from_anna/hdf5-ize_dataset.py
+++ scripts/from_anna/hdf5-ize_dataset.py
@@ -2,7 +2,7 @@ import h5py
 import numpy as np
 import uproot as ur
 import awkward as ak
-import torch
+#import torch
 import matplotlib.pyplot as plt
 
 
@@ -33,33 +33,35 @@ def get_comp_from_root(rootf, prec, next_):
     image_sup_branch = tree['_image_v']
     image_branch = tree['_image_v/_image_v._img']
 
-    branch_entries = ak.to_numpy(image_branch.array(entry_start=prec, entry_stop=next_))
-    linear_data = branch_entries[0][0]
+    branch_entries = image_branch.array(entry_start=prec, entry_stop=next_)
+    linear_data = branch_entries[0]
 
     image_meta = get_meta(image_sup_branch)
-    col_count = image_sup_branch[image_meta['col_count']].array(entry_start=prec, entry_stop=next_)[0][0]
-    row_count = image_sup_branch[image_meta['row_count']].array(entry_start=prec, entry_stop=next_)[0][0]
-    mdata = linear_data.reshape(col_count, row_count)
-    nz_indices = np.transpose(np.nonzero(mdata))
-    nz_x = [index[0] for index in nz_indices]
-    nz_y = [index[1] for index in nz_indices]
-    nz_values = mdata[nz_indices[:, 0], nz_indices[:, 1]]
+    col_count = image_sup_branch[image_meta['col_count']].array(entry_start=prec, entry_stop=next_)[0]
+    row_count = image_sup_branch[image_meta['row_count']].array(entry_start=prec, entry_stop=next_)[0]
+    result = []
+    for i in range(3) :
+        mdata = ak.to_numpy(linear_data[i]).reshape(col_count[i], row_count[i])
+        nz_indices = np.transpose(np.nonzero(mdata))
+        nz_x = [index[0] for index in nz_indices]
+        nz_y = [index[1] for index in nz_indices]
+        nz_values = mdata[nz_indices[:, 0], nz_indices[:, 1]]
+        result.append((nz_x, nz_y, nz_values))
+    return result
 
-    return nz_x, nz_y, nz_values
 
+# def coo_to_torch(hdf5_path, image_key):
+#     with h5py.File(hdf5_path, 'r') as hdf5_file:
+#         if image_key in hdf5_file:
+#             dset = hdf5_file[image_key]
+#             nz_x = dset['nz_x'][:]
+#             nz_y = dset['nz_y'][:]
+#             values = torch.FloatTensor(dset['value'][:])
+#             indices = torch.LongTensor([nz_x, nz_y])
+#             rows, cols = max(nz_x) + 1, max(nz_y) + 1
+#             sp_tensor = torch.sparse_coo_tensor(indices, values, torch.Size([rows, cols]))
 
-def coo_to_torch(hdf5_path, image_key):
-    with h5py.File(hdf5_path, 'r') as hdf5_file:
-        if image_key in hdf5_file:
-            dset = hdf5_file[image_key]
-            nz_x = dset['nz_x'][:]
-            nz_y = dset['nz_y'][:]
-            values = torch.FloatTensor(dset['value'][:])
-            indices = torch.LongTensor([nz_x, nz_y])
-            rows, cols = max(nz_x) + 1, max(nz_y) + 1
-            sp_tensor = torch.sparse_coo_tensor(indices, values, torch.Size([rows, cols]))
-
-            return sp_tensor
+#             return sp_tensor
 
 
 def create_hdf5_from_root(rootf_path, hdf5_path):
@@ -69,17 +71,18 @@ def create_hdf5_from_root(rootf_path, hdf5_path):
         ('value', np.float32)])
 
     with ur.open(rootf_path) as rootf:
+        with h5py.File(hdf5_path, 'a') as hdf5_file:
 
-        for i in range(10):
-            nz_x, nz_y, nz_values = get_comp_from_root(rootf, i, i + 1)
-
-            components = np.array(list(zip(nz_x, nz_y, nz_values)), dtype=dtype)
-
-            with h5py.File(hdf5_path, 'a') as hdf5_file:
-                dset_name = f"image_{i + 1}"
-                if dset_name not in hdf5_file:
-                    hdf5_file.create_dataset(dset_name, data=components, compression='gzip',
-                                             compression_opts=4, chunks=True)
+            for i in range(10):
+                by_plane = get_comp_from_root(rootf, i, i + 1)
+                iplane = -1
+                for nz_x, nz_y, nz_values in by_plane :
+                    iplane += 1
+                    components = np.array(list(zip(nz_x, nz_y, nz_values)), dtype=dtype)
+                    dset_name = f"image_plane{iplane}_{i + 1}"
+                    if dset_name not in hdf5_file:
+                        hdf5_file.create_dataset(dset_name, data=components, compression='gzip',
+                                                 compression_opts=4, chunks=True)
 
 
 def print_hdf5_dirtree(hdf5_path):
@@ -116,9 +119,15 @@ def plot_im_from_hdf5(hdf5_path, image_key):
 
 
 if __name__ == '__main__':
-    rootf_path = '/Users/hardie/research/protondecay_hA_BodekRitchie_dune10kt_1x2x6_54474279_179_20220423T063923Z_gen_g4_detsim_reco_65804491_0_20230126T175412Z_reReco_larcv_10.root'
-    hdf5_path = '/Users/hardie/sample_root.hdf5'
+    import sys, os
+
+    rootf_path = sys.argv[1]
+    hdf5_path = sys.argv[2]
+
+    print(f'{rootf_path = }')
+    print(f'{hdf5_path = }')
     create_hdf5_from_root(rootf_path, hdf5_path)
+
 
     # print_hdf5_dirtree(hdf5_path)
     # plot_im_from_hdf5(hdf5_path, 'image_2')
